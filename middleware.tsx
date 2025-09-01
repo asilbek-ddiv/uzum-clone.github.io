a72a21983afbe78ae91884a82eeb2e157f8e88ff
Asilbek Dev <asilbekegamnazarov999@gmail.com>
2025-09-01 23:10:58 +0500

    add

--- middleware.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const middleware = () => {
-  return (
-    <div>middleware</div>
-  )
-}
-
-export default middleware
---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://uzum-market-online.vercel.app',
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+    outDir: './public',
+};
