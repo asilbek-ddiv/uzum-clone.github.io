8081da5678d1d6a848ce9b9093221476ab1af9d3
Asilbek Dev <asilbekegamnazarov999@gmail.com>
2025-09-02 08:40:30 +0500

    add

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -2,13 +2,75 @@ import React from 'react'
 
 const page = () => {
     return (
-        <div className='containers'>
-            <div className='flex gap-5 w-full min-h-screen'>
-                <aside className='flex flex-col w-1/6'>
-        
-                </aside>
-                <div>
-
+        <div className='containers mt-5'>
+            <div className='flex flex-col gap-2.5 w-full min-h-screen'>
+                <h2 className='text-3xl pl-2.5'>Savollar va javoblar</h2>
+                <div className='w-full flex justify-between'>
+                    <aside className='flex flex-col gap-2.5 w-[19%]'>
+                        <h3 className='text-md font-semibold pl-2.5'>Ro'yxatdan o'tish</h3>
+                        <a href="#" className='text-md p-2 rounded hover:bg-gray-200 transition-all'>Qanday qilib ilovada ro'yxatdan o'tish mumkin?</a>
+                        <a href="#" className='text-md p-2 rounded hover:bg-gray-200 transition-all'>Qanday qilib saytda ro'yxatdan o'tish mumkin?</a>
+                        <a href="#" className='text-md p-2 rounded hover:bg-gray-200 transition-all'>Uzum ID nima?</a>
+                        <a href="#" className='text-md p-2 rounded hover:bg-gray-200 transition-all'>Menda Uzum ID bormi?</a>
+                        <a href="#" className='text-md p-2 rounded hover:bg-gray-200 transition-all'>Uzum ID yordamida qanday kirish mumkin?</a>
+                        <a href="#" className='text-md p-2 rounded hover:bg-gray-200 transition-all'>Uzum ID bilan kirish xavfsizmi?</a>
+                        <h3 className='text-md font-semibold pl-2.5'>Buyurtmani rasmiylashtirish</h3>
+                        <h3 className='text-md font-semibold pl-2.5'>Yetkazib berish</h3>
+                        <h3 className='text-md font-semibold pl-2.5'>To'lov</h3>
+                        <h3 className='text-md font-semibold pl-2.5'>Uzum Nasiya muddatli to'lovi</h3>
+                        <h3 className='text-md font-semibold pl-2.5'>Buyurtmani qabul qilish</h3>
+                        <h3 className='text-md font-semibold pl-2.5'>Mahsulotlar</h3>
+                        <h3 className='text-md font-semibold pl-2.5'>Sifatiga e'tiroz bo'lmagan mahsulotni qaytarish</h3>
+                        <h3 className='text-md font-semibold pl-2.5'>Sifatiga e'tiroz bo'lgan mahsulotni qaytarish</h3>
+                        <h3 className='text-md font-semibold pl-2.5'>To'lovni qaytarish</h3>
+                        <h3 className='text-md font-semibold pl-2.5'>Sharhlar</h3>
+                        <h3 className='text-md font-semibold pl-2.5'>Internet firibgarlar</h3>
+                        <h3 className='text-md font-semibold pl-2.5'>Arzon narx kafolati aksiyasi</h3>
+                        <h3 className='text-md font-semibold pl-2.5'>Sotuvchi yetkazib berishi</h3>
+                        <h3 className='text-md font-semibold pl-2.5'>Savollaringiz bormi?</h3>
+                    </aside>
+                    <div className='flex flex-col border w-[80%] rounded p-5 gap-2.5'>
+                        <div>
+                            <h2 className='text-xl font-semibold'>Ro'yxatdan o'tish</h2>
+                            <p>Buyurtmani rasmiylashtirib to’lovni bajarish uchun siz ro'yxatdan o’tishingiz kerak bo'ladi. Uning yordamida ma'lumotlarni doimiy ravishda to'ldirishingiz shart bo’lmaydi. Ro'yxatdan o'tish vaqti 3 daqiqadan oshmaydi.</p>
+                        </div>
+                        <h3 className='text-md font-semibold'>Qanday qilib ilovada ro'yxatdan o'tish mumkin?</h3>
+                        <div>
+                            <p>Uzum Market ilovasini yuklab oling:</p>
+                            <p>- App Storeda: <a href="https://clck.ru/367gQC" className='hover:text-blue-500 transition-all underline'>https://clck.ru/367gQC</a>,</p>
+                            <p>- App Storeda: <a href="https://clck.ru/367gTR" className='hover:text-blue-500 transition-all underline'>https://clck.ru/367gQC</a>.</p>
+                        </div>
+                        <p>Ilovaga kirib «Kirish» tugmasini bosing va telefon raqamingizni kiriting, «Kodni qabul qilish» tugmasini bosing. Sizga 4 xonali tasdiqlash kodi keladi, kodni kiritasiz va o’z shaxsiy kabinetingizga kirasiz.</p>
+                        <div>
+                            <p>So’ng, ekranni yuqori o’ng qismidagi sozlama tugmasini ⚙️ bosasiz va ushbu ma’lumotlarni kiritasiz:</p>
+                            <p>- Familiya, ism, sharif;</p>
+                            <p>- Telefon raqami;</p>
+                            <p>- Elektron pochta manzili(agar istasangiz);</p>
+                            <p>- Tug’ilgan sana;</p>
+                            <p>- Jinsingizni kiritib «Saqlash» tugmasini bosasiz.</p>
+                        </div>
+                        <p>Shaxsiy kabinetdan chiqib ketish uchun, kabinet past qismida joylashgan «Chiqish» tugmasini bosasiz.</p>
+                        <h3 className='text-md font-semibold'>Qanday qilib saytda ro’yxatdan o’tish mumkin?</h3>
+                        <p>Bizning <a href="https://uzum.uz/uz" className='hover:text-blue-500 transition-all underline'>uzum.uz</a> sahifamizga kirib «Kirish» tugmasini bosing, bu ekranning o’ng taraf yuqori qismda bo’ladi. Telefon raqamingizni kiriting, «Kodni qabul qilish» tugmasini bosing. Sizga 4 xonali tasdiqlash kodi keladi, kodni kiritasiz va o’z shaxsiy kabinetingizga kirasiz.</p>
+                        <div>
+                            <p>So’ng, ekranni yuqori chap qismida sozlama tugmasini ⚙️ bosasiz va ushbu ma’lumotlarni kiritasiz:</p>
+                            <p>- Familiya, ism, sharif;</p>
+                            <p>- Telefon raqami;</p>
+                            <p>- Elektron pochta manzili(agar istasangiz);</p>
+                            <p>- Tug’ilgan sana;</p>
+                            <p>- Jinsingizni kiritib «Saqlash» tugmasini bosasiz.</p>
+                        </div>
+                        <p>Shaxsiy kabinetdan chiqib ketish uchun, o’ng qismda joylashgan ismingiz ustiga bosasiz va «Sozlamalar» qismiga kirib «Tizimdan chiqib ketish» tugmasini bosasiz.</p>
+                        <h3 className='text-md font-semibold'>Uzum ID nima?</h3>
+                        <p>Bu Uzum xizmatlariga kirish uchun yagona avtorizatsiya xizmatidir.</p>
+                        <h3 className='text-md font-semibold'>Menda Uzum ID bormi?</h3>
+                        <p>Agar siz Uzum Market, Uzum Bank yoki Uzum Tezkordan foydalangan bo‘lsangiz - demak, sizda Uzum ID mavjud, qo‘shimcha hech narsa rasmiylashtirish yoki yaratishning hojati yo‘q.</p>
+                        <h3 className='text-md font-semibold'>Uzum ID yordamida qanday kirish mumkin?</h3>
+                        <p>Buning uchun Uzum Market, Uzum Bank yoki Uzum Tezkor ilovalarini ishga tushirish, telefon raqamini kiritish va SMS orqali kirishni tasdiqlash kifoya.</p>
+                        <h3 className='text-md font-semibold'>Uzum ID bilan kirish xavfsizmi?</h3>
+                        <p>Ha, Uzum IDdagi ma’lumotlaringiz ishonchli himoyalangan — biz zamonaviy shifrlash usullaridan foydalanamiz va shaxsiy ma’lumotlarni himoya qilish bo‘yicha qonunchilikning barcha talablariga rioya qilamiz.</p>
+                        <h2 className='text-xl font-semibold'>Buyurtmani rasmiylashtirish</h2>
+                    </div>
                 </div>
             </div>
         </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
+import React from "react";
 import type { Metadata } from "next";
+import { Inter } from "next/font/google"
 import "./globals.css";
 import Navbar from "@/components/layout/header/Navbar";
 import Footer from "@/components/layout/footer/footer";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://uzum-market-online.vercel.app"),
@@ -46,7 +50,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="uz" suppressHydrationWarning>
-      <body>
+      <body className={inter.className}>
         <Navbar />
         <main>{children}</main>
         <Footer />
