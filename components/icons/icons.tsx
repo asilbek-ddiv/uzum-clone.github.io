d2349eab175d96a03f456d385ad1bc956a17348b
Asilbek <asilbekegamnazarov999@gmail.com>
2025-11-08 23:43:48 +0500

    add

---
+++ components/icons/icons.tsx
@@ -0,0 +1,35 @@
+import { 
+    ChevronDown,
+     ChevronLeft,
+      ChevronRight, 
+      ChevronUp, 
+      Facebook, 
+      GalleryVerticalEnd,
+       Heart, 
+       Instagram, 
+       MapPin, 
+       Search, 
+       ShoppingBag, 
+       ShoppingBasket, 
+       User, 
+       Youtube
+    } from "lucide-react"
+
+export const MapIcon=()=><MapPin />
+
+export const ChevronUpIcon=()=><ChevronUp />
+export const ChevronRightIcon=()=> <ChevronRight/>
+export const ChevronDownIcon=()=><ChevronDown />
+export const ChevronLeftIcon=()=> <ChevronLeft/>
+
+export const CatalogIcon=()=><GalleryVerticalEnd />
+export const SearchIcon=()=><Search />
+export const UserIcon=()=><User />
+export const WishesIcon=()=> <Heart />
+export const CartIcon=()=> <ShoppingBag/>
+export const ShoppingIcon=()=> <ShoppingBasket />
+
+export const InstagramIcon=()=> <Instagram />
+export const TelegramIcon=()=> <Telegram />
+export const FacebookIcon=()=> <Facebook />
+export const YouTubeIcon=()=> <Youtube />
